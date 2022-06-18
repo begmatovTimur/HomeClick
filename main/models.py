846b4bc25ebff212859bd31f50335e6909f06578
begmatovTimur <tbegmatov40@gmail.com>
2022-06-18 18:48:24 +0500

    Create a new model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+DOLLAR = "Доллар"
+SOUM = "Сум"
+EURO = "Евро"
+CURRENCY = [
+    (DOLLAR, (DOLLAR)),
+    (SOUM, (SOUM)),
+    (EURO, (EURO)),
+]
+
+LAND = "Участок"
+FLAT = "Квартира"
+ESTATE_TYPE = [
+    (LAND, ('Участок недвижимости')),
+    (FLAT, ('Квартира')),
+]
+
+
+class Estate(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=255, null=True, blank=True) 
+    description = models.TextField(verbose_name="Описание", null=True, blank=True)
+    price = models.PositiveBigIntegerField(verbose_name="Цена")
+    currency = models.CharField(verbose_name="Валюта", max_length=32, choices=CURRENCY, default=DOLLAR)
+    area = models.PositiveIntegerField(verbose_name="Площадь")
+    room_amount = models.PositiveSmallIntegerField(verbose_name="Количество комнат")
+    type = models.CharField(verbose_name="Тип недвижимости", max_length=64, choices=ESTATE_TYPE, default=FLAT)
+
+    def get_price_per_meter(self):
+        return f'{self.price / self.area} м²'
+
+    def get_title(self):
+        return self.title if self.title else f'{self.area} м², {self.room_amount} комнат'
+
+    def __str__(self):
+        return self.get_title()
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Estate
 
 
 def index(request):
-    return render(request, "index.html")
+    estates = Estate.objects.all()
+    return render(request, "index.html", {'estates': estates})
