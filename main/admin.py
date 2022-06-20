aeec2418515973bd42ec5e29d219252197b2105c
muhach12345678910 <muhachibadullaev@gmail.com>
2022-06-20 13:01:23 +0500

    add category model in project

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from main.models import Estate
+from main.models import Estate, Category
 
 
 admin.site.register(Estate)
+admin.site.register(Category)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Estate
+from .models import Estate, Category
 
 
 def index(request):
     estates = Estate.objects.all()
-    return render(request, "index.html", {'estates': estates})
+    categories = Category.objects.all()
+    return render(request, "index.html", {'estates': estates, 'categories': categories})
