f3b1c7fdde88db2e4f68f40aa719b6c73a1ec70f
muhach12345678910 <muhachibadullaev@gmail.com>
2022-06-21 16:53:26 +0500

    add bootstrap

--- authentication/register_form.py
+++ authentication/register_form.py
@@ -14,32 +14,32 @@ class SignUpForm(UserCreationForm):
         help_text='Необходимый. 150 символов или меньше. Только буквы, цифры и @/./+/-/_.',
 
         error_messages={'unique': "Пользователь с таким именем уже существует."},
-        widget=forms.TextInput(attrs={'class': 'register_input', 'placeholder': 'Имя ползователья'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя ползователья'})
     )
 
     first_name = forms.CharField(
         max_length=12, 
         min_length=4, 
         required=True,
-        widget=forms.TextInput(attrs={'class': 'register_input', 'placeholder': 'Имя'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'})
     )
     last_name = forms.CharField(
         max_length=12, 
         min_length=4, 
         required=True,
-        widget=(forms.TextInput(attrs={'class': 'register_input', 'placeholder': 'Фамилия'}))
+        widget=(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}))
     )
     email = forms.EmailField(
         max_length=50, 
         help_text='Необходимый. Сообщите действующий адрес электронной почты.',                     
-        widget=(forms.TextInput(attrs={'class': 'register_input', 'placeholder': 'Email'}))
+        widget=(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     )
     password1 = forms.CharField(
         label='Password',
-        widget=(forms.PasswordInput(attrs={'class': 'register_input', 'placeholder': 'Пароль'}))
+        widget=(forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
     )
     password2 = forms.CharField(
         label='Password Confirmation', 
-        widget=forms.PasswordInput(attrs={'class': 'register_input', 'placeholder': 'Повторите пароль'}),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Повторите пароль'}),
         help_text='Просто введите тот же пароль, для подтверждения'
     )
