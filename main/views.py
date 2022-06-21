40135f743f1fccf7e58b3514c6697a871628c57c
begmatovTimur <tbegmatov40@gmail.com>
2022-06-21 09:07:45 +0500

    Add styles in home page

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Estate, Category
+from django.db.models import Q
 
 
 def index(request):
     estates = Estate.objects.all()
+    search_estates = request.GET.get("search")
+    if search_estates:
+        estates = Estate.objects.filter(Q(description__icontains=search_estates) and Q(price__icontains=search_estates) and Q(room_amount__icontains=search_estates))
+        
     categories = Category.objects.all()
     return render(request, "index.html", {'estates': estates, 'categories': categories})
